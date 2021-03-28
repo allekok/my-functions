8607158418e2b999c1b7c52954cf0cd2906b79e9
Payam <payambapiri.97@gmail.com>
2021-03-28 13:50:41 +0430

    srv/my-library.php: "mb_substr" -> "substr" in "download" function.

--- srv/my-library.php
+++ srv/my-library.php
@@ -1,10 +1,7 @@
 <?php
 function download($url) {
-	if(mb_substr($url, 0, 8) == "https://" or
-		mb_substr($url, 0, 7) == "http://")
-	{
+	if(substr($url, 0, 8) == "https://" or substr($url, 0, 7) == "http://")
 		return @file_get_contents($url);
-	}
 	return "E: Invalid Url.";
 }
 ?>
