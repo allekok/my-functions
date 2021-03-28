cb6c4c0dd34624bfa467608bd0580ec435926a32
Payam <payambapiri.97@gmail.com>
2021-03-29 00:00:45 +0430

    srv/my-library.php: Adding "aparat" function + Some necessary additional functions.

--- srv/my-library-functions.php
+++ srv/my-library-functions.php
@@ -1,5 +1,9 @@
 <?php
 const my_library_functions = [
+	'aparat($url)',
 	'download($url)',
+	'get_element_by_class($parent, $class, $tag="*", $i=0)',
+	'get_elements_by_class($parent, $class, $tag = "*")',
+	'parse_html($html)',
 ];
 ?>
--- srv/my-library.php
+++ srv/my-library.php
@@ -4,4 +4,46 @@ function download($url) {
 		return @file_get_contents($url);
 	return "E: Invalid Url.";
 }
+
+function parse_html($html) {
+	$dom = new DOMDocument;
+	@$dom->loadHTML($html);
+	return $dom;
+}
+
+function get_elements_by_class($parent, $class, $tag = "*") {
+	$elements = [];
+	foreach($parent->getElementsByTagName($tag) as $el) {
+		$el_class = $el->getAttribute("class");
+		$class_list = preg_split("/\s+/u", $el_class,
+					-1, PREG_SPLIT_NO_EMPTY);
+		if(in_array($class, $class_list))
+			$elements[] = $el;
+	}
+	return $elements;
+}
+
+function get_element_by_class($parent, $class, $tag="*", $i=0) {
+	return get_elements_by_class($parent, $class, $tag)[$i];
+}
+
+function aparat($url) {
+	$result = [];
+	
+	$html = download($url);
+	$dom = parse_html($html);
+
+	$download_dropdown = get_element_by_class($dom, "download-dropdown");
+	$qualities = $download_dropdown->getElementsByTagName("li");
+
+	foreach($qualities as $quality) {
+		$a = $quality->getElementsByTagName("a")[0];
+		$link = $a->getAttribute("href");
+		$label = $a->getAttribute("aria-label");
+		$label = str_replace("با کیفیت", "", $label);
+		$label = trim($label);
+		$result[$label] = $link;
+	}
+	return $result;
+}
 ?>
